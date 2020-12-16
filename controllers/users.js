8268651dac412aa82bff23afabd898af0fbed8c7
Amory <garbage90@yandex.ru>
2020-12-16 21:34:12 +0300

    getUserById

--- controllers/users.js
+++ controllers/users.js
@@ -9,8 +9,30 @@ getUser = (req,res) => {
 getUserById = (req,res) => {
   const {id} = req.params;
   UserSchema.findById(id)
+    .orFail(() => {
+      const error = new Error('Пользователь не найден');
+      error.statusCode = 404;
+      throw error;
+    })
     .then(data => res.status(200).send(data))
-    .catch(err => res.status(500).send({message: 'Пользователь не найден'}))
+    //.catch(err => res.status(500).send({message: err}))
+    // .catch((err) => {
+    //   if (err.kind === 'ObjectId') {
+    //     res.status(400).send({message: 'Ошибка получения данных'});
+    //   } else {if (err.statusCode === 404) {
+    //     res.status(404).send({message: err.message})
+    //   } else {
+    //     res.status(500).send('Ошибка сервера');
+    //   }
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        res.status(400).send({message: 'Ошибка получения данных'});
+      } else if (err.statusCode === 404) {
+        res.status(404).send({message: err.message})
+      } else {
+        res.status(500).send({message: 'Ошибка сервера'});
+      }
+    })
 }
 
 createUser = (req,res) => {
